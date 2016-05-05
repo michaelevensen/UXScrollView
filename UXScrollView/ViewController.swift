7dfc1a48f38a25b01df8d58d37f802a15093b074
Michael Nino Evensen <michaelevensen@gmail.com>
2016-05-05 16:53:31 +0200

    example of two UICollectionView embeds. Note for this to work set the ContentView height to be fixed to a total, and then set the Bottom Space to be 0, not a minus value.

--- UXScrollView/ViewController.swift
+++ UXScrollView/ViewController.swift
@@ -8,23 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+private let reuseIdentifier = "Cell"
 
-    @IBOutlet weak var textLabel1: UILabel!
-    @IBOutlet weak var textLabel2: UILabel!
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // dummy text
-        let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla mauris at tincidunt imperdiet. Nam ultrices nec augue et fringilla. Mauris eu lacus non nulla efficitur blandit. Ut ac fermentum urna. Nunc blandit, nisi eu suscipit dignissim, nisl lacus vulputate arcu, in rhoncus tellus justo nec enim. Fusce dictum elit vulputate, ultrices metus a, fermentum felis. Morbi a ligula a augue luctus dignissim et et massa. Etiam vel justo ac enim eleifend commodo vel eget diam. Curabitur semper tincidunt justo, id condimentum felis tristique vel. Mauris ornare vitae lorem sit amet fermentum. Donec congue leo erat, non facilisis turpis consequat eget. Cras dapibus ullamcorper semper."
-        
-
-        // set text
-        textLabel1.text = text
-        textLabel2.text = text
-        
     }
+    
+    // MARK: UICollectionViewDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath)
+        
+        return cell
+    }
+
+    
+    
     
     
 
